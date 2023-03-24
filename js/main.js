64f89a5097e324a6656e2a5625f854c2a332a618
Amit Rawat <rawat3209@gmail.com>
2023-03-24 15:32:28 +0530

    work on inner pages

---
+++ js/main.js
@@ -0,0 +1,42 @@
+//slider testimonials start
+
+$('.slider-testimonials').slick({
+    infinite: true,
+    slidesToShow: 3,
+    prevArrow: $(".customprev"),
+    nextArrow: $(".customnext"),
+    autoplaySpeed: 1000,
+    dots: true,
+    slidesToScroll: 1,
+    responsive: [{
+            breakpoint: 991,
+            settings: {
+                slidesToShow: 2,
+                dots: false,
+                arrows: true,
+                slidesToScroll: 1
+            }
+        },
+
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                dots: false,
+                arrows: true,
+                slidesToScroll: 1
+            }
+        },
+
+    ]
+
+
+});
+
+on('setPosition', function (event, slick) {
+    slick.$slides.css('height', slick.$slideTrack.height() + 'px');
+});
+
+
+
+    //slider testimonials end
