53878ff16190364679069edfcb7b4c04de589d80
mail2ssr <ssr@sanrachana.com>
2023-04-21 18:03:16 +0530

    ssr

--- blocks/blue_background_content_section.php
+++ blocks/blue_background_content_section.php
@@ -6,16 +6,14 @@ if (get_sub_field('hide_this_block') == 0) { ?>
     <section class="c-blue-w">
         <div class="container">
             <div class="cont-blue os-animation" data-os-animation="fadeInUp" data-os-animation-delay=".5s">
-            <?php the_sub_field( 'content' ); ?>
+                <?php the_sub_field('content'); ?>
 
-            <?php $bottom_image = get_sub_field( 'bottom_image' ); ?>
-			<?php if ( $bottom_image ) : ?>
-				<div class="img-btm"><img src="<?php echo esc_url( $bottom_image['url'] ); ?>" alt="<?php echo esc_attr( $bottom_image['alt'] ); ?>" /></dv >
-			<?php endif; ?>
+                <?php $bottom_image = get_sub_field('bottom_image'); ?>
+                <?php if ($bottom_image) : ?>
+                    <div class="img-btm"><img src="<?php echo esc_url($bottom_image['url']); ?>" alt="<?php echo esc_attr($bottom_image['alt']); ?>" /></div>
+                <?php endif; ?>
             </div>
-        </div>
         </div>
     </section>
     <!--Common Blue End-->
-
 <?php } ?>
