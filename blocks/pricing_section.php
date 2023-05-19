599d4cb31c15d1e07fc694f110c74b35e8da7f73
mail2ssr <ssr@sanrachana.com>
2023-05-19 14:10:26 +0530

    ssr

--- blocks/pricing_section.php
+++ blocks/pricing_section.php
@@ -1,33 +1,12 @@
 <?php
-$media_position = get_sub_field('media_position');
-$select_media_type = get_sub_field('select_media_type');
-
-
 
 if (get_sub_field('hide_this_block') == 0) { ?>
 
-
-    <!--Home Banner Start-->
-    <section class="home-banner-w">
+    <!-- Free Trial Header Start-->
+    <section class="pricing-header">
         <div class="container">
-            <div class="row align-items-<?php the_sub_field('vertical_align'); ?>">
-                <div class="col-lg-5 order-lg-1">
-                    <div class="home-video-img os-animation" data-os-animation="fadeInDown" data-os-animation-delay=".5s">
-                        <?php if ($select_media_type == 'image') {
-                            get_template_part('blocks/inc/image-inc');
-                        }
-                        ?>
-                        <?php if ($select_media_type == 'video') {
-                            get_template_part('blocks/inc/video-inc');
-                        }
-                        ?>
-                        <?php if ($select_media_type == 'json') {
-                            get_template_part('blocks/inc/jsonAnimation-inc');
-                        }
-                        ?>
-                    </div>
-                </div>
-                <div class="col-lg-7 order-lg-0 d-flex">
+            <div class="row">
+                <div class="col-lg-6">
                     <div class="home-info os-animation" data-os-animation="fadeInDown" data-os-animation-delay=".5s">
                         <?php the_sub_field('content'); ?>
 
@@ -35,7 +14,6 @@ if (get_sub_field('hide_this_block') == 0) { ?>
                         <?php $phone_number = get_sub_field('phone_number'); ?>
                         <?php if ($button_1 || $phone_number) : ?>
                             <div class="actions">
-
                                 <?php if ($button_1) : ?>
                                     <div class="green-button-i"><a href="<?php echo esc_url($button_1['url']); ?>" target="<?php echo esc_attr($button_1['target']); ?>"><?php echo esc_html($button_1['title']); ?></a></div>
                                 <?php endif; ?>
@@ -44,6 +22,9 @@ if (get_sub_field('hide_this_block') == 0) { ?>
                                 <?php endif; ?>
                             </div>
                         <?php endif; ?>
+
+
+
 
                         <?php if (have_rows('logo_group')) : ?>
                             <div class="trusted-partners">
@@ -59,9 +40,21 @@ if (get_sub_field('hide_this_block') == 0) { ?>
                         <?php endif; ?>
                     </div>
                 </div>
+                <div class="col-lg-5 offset-lg-1">
+                    <div class="form-freetrial">
+                        <?php the_sub_field('form_section'); ?>
+
+                        <div class="form">
+                            <?php
+                            $formCode = get_sub_field('form_shortcode');
+                            echo do_shortcode($formCode);
+                            ?>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
-    <!--Home Banner End-->
+    <!-- Free Trial Header End-->
 
 <?php } ?>
